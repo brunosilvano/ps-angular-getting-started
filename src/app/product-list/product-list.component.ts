248ce1f489ee9ca5eea447f1d981509655b36d5c
Bruno Silvano Costa <bruno_705@hotmail.com>
2017-10-08 23:17:06 -0300

    added show/hide images feature

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,6 +9,7 @@ export class ProductListComponent implements OnInit {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
+  showImage: boolean = false;
   products: any[] = [
     {
       'productId': 1,
@@ -31,6 +32,10 @@ export class ProductListComponent implements OnInit {
       'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
     }
   ];
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 
   constructor() { }
 
